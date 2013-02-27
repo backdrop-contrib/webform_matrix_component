62b43170a960609faee9493764da486d79eae47d
cpsinghal <chetansinghal1988@gmail.com>
2013-02-27 14:58:33 +0530

    some changes

---
+++ README.txt
@@ -0,0 +1,15 @@
+
+Webform Matrix Component
+
+************************
+
+This module adds the ability to create matrix component.
+This module also give flaxibility to choose any type of element like textfield,select,label,date
+
+Admin settings-admin/config/content/webform
+You can set max no row and max no columns.
+
+
+
+
+
--- webform_matrix_component.js
+++ webform_matrix_component.js
@@ -5,27 +5,57 @@
 
 (function ($) {
 
-	show_hide_element_type = function(elementid){
+Drupal.behaviors.webform_matrix = Drupal.behaviors.webform_matrix || {};
 
-		var $type=$('#edit-extra-element-'+elementid+'-type').val();
-		switch($type){
-			case 'textfield': 
-			case 'date':    
-				$('.parent-form-item-extra-element-'+elementid+'-label-name').hide();
-				$('.parent-form-item-extra-element-'+elementid+'-option').hide();
-				break;
+Drupal.behaviors.webform_matrix.attach = function(context) {
+  // Calendar datepicker behavior.
+  Drupal.webform_matrix.datepicker(context);
+  
+};
 
-			case 'select':
-				var label_div='form-item-extra-element-'+elementid+'-label-name';
-				console.log(label_div+' #form-item-element-element-1-label-name');
-				$('.parent-form-item-extra-element-'+elementid+'-option').show();
-				$('.parent-form-item-extra-element-'+elementid+'-label-name').hide();
-				break;
+Drupal.webform_matrix = Drupal.webform_matrix || {};
 
-			case 'label':
-				$('.parent-form-item-extra-element-'+elementid+'-label-name').show();
-				$('.parent-form-item-extra-element-'+elementid+'-option').hide();
-				break;
+Drupal.webform_matrix.datepicker = function(context) {
+	
+	$('.webform-matrix-calendar').each(function() {
+		var $calendar=this;
+		
+		var startDate = $calendar.className.replace(/.*webform-calendar-start-(\d{4}-\d{2}-\d{2}).*/, '$1').split('-');
+		var endDate = $calendar.className.replace(/.*webform-calendar-end-(\d{4}-\d{2}-\d{2}).*/, '$1').split('-');
+		var firstDay = $calendar.className.replace(/.*webform-calendar-day-(\d).*/, '$1');
+		var defaultDay=$calendar.className.replace(/.*webform-calendar-defaultDay-(\d).*/, '$1');
+		// Convert date strings into actual Date objects.
+		startDate = new Date(startDate[0], startDate[1] - 1, startDate[2]);
+		endDate = new Date(endDate[0], endDate[1] - 1, endDate[2]);
+
+		// Ensure that start comes before end for datepicker.
+		if (startDate > endDate) {
+		  var laterDate = startDate;
+		  startDate = endDate;
+		  endDate = laterDate;
 		}
-    }
+
+		var startYear = startDate.getFullYear();
+		var endYear = endDate.getFullYear();
+
+		// Set up the jQuery datepicker element.
+		$(this).datepicker({
+		  dateFormat: 'yy-mm-dd',
+		  yearRange: startYear + ':' + endYear,
+		  firstDay: parseInt(firstDay),
+		  minDate: startDate,
+		  maxDate: endDate,
+		  changeMonth:true,
+		  changeYear:true,
+		  Default:defaultDay
+		});
+		  
+		// Prevent the calendar button from submitting the form.
+		$calendar.click(function(event) {
+		  $(this).focus();
+		  event.preventDefault();
+		});
+	});
+}
+	
 }) (jQuery);
