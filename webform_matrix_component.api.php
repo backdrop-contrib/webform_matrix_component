77b8c70ad635c603197ff55b4ed3eaa0055538be
cpsinghal <chetansinghal1988@gmail.com>
2013-03-04 15:48:36 +0530

    create alter

--- webform_matrix_component.api.php
+++ webform_matrix_component.api.php
@@ -34,4 +34,24 @@ function webform_render_matrix_textfield_Alphabet($element, $form_state) {
 }
 
 
+function hook_webform_render_matrix_alter(&$component,&$value,&$element){
+    $helptext = $value['helptext']; 
+    if (!empty($helptext)) {
+         $element['#field_suffix'] = $helptext;
+    }
+}
+
+function hook_webform_edit_matrix_alter(&$element,&$element_values,$element_id){
+     $helptext = @$form_element_values['helptext'];
+    $form_element['helptext'] = array(
+        '#type' => 'textfield',
+        '#title' => 'Help Text',
+        '#size' => 15,
+        '#default_value' => $helptext,
+        '#parents' => array('extra', 'element', $element_id, 'helptext'),
+        '#weight'=>1,
+    );
+}
+
 ?>
+
