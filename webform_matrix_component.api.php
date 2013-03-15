f13e1f6e5bc2244d88d62a87852666ac94f1a07d
cpsinghal <chetansinghal1988@gmail.com>
2013-03-15 17:11:23 +0530

    remove js

--- webform_matrix_component.api.php
+++ webform_matrix_component.api.php
@@ -7,8 +7,10 @@
 
 /**
  * validate hook for matrix component
+ * you can define custom validation for this
  * @param type $element
  * @param type $form_state
+ * 
  */
 function hook_webform_matrix_validate($element, $form_state) {
     
@@ -16,6 +18,7 @@ function hook_webform_matrix_validate($element, $form_state) {
 
 /**
  * Validate textfield Numeric validation
+ * @see webform_render_matrix_textfield_Numeric
  */
 function webform_render_matrix_textfield_Numeric($element, $form_state) {
     $value = $element['#value'];
@@ -28,23 +31,20 @@ function webform_render_matrix_textfield_Numeric($element, $form_state) {
 
 /**
  * Validate textfield Numeric validation
+ * @see webform_render_matrix_textfield_Numeric
  */
 function webform_render_matrix_textfield_Alphabet($element, $form_state) {
     
 }
 
-function hook_webform_render_matrix_alter(&$component, &$value, &$element) {
-    $helptext = $value['helptext'];
-    if (!empty($helptext)) {
-        $element['#field_suffix'] = $helptext;
-    }
-}
-
 /**
- * 
+ * webform_edit_matrix_alter for alter matrix edit form
+ * like add helptext textfield
+ * @see hook_webform_render_matrix_alter
  * @param type $element
  * @param type $element_values
  * @param type $element_id
+ * 
  */
 function hook_webform_edit_matrix_alter(&$element, &$element_values, $element_id) {
     $helptext = $element_values['helptext'];
@@ -57,6 +57,22 @@ function hook_webform_edit_matrix_alter(&$element, &$element_values, $element_id
         '#weight' => 1,
     );
 }
+
+/**
+ * webform_render_matrix_alter for alter webform_render_matrix 
+ * @see hook_webform_edit_matrix_alter
+ * @param type $component
+ * @param type $value
+ * @param type $element
+ */
+function hook_webform_render_matrix_alter(&$component, &$value, &$element) {
+    $helptext = $value['helptext'];
+    if (!empty($helptext)) {
+        $element['#field_suffix'] = $helptext;
+    }
+}
+
+
 ?>
 
 
