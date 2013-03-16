479208a095327c385401b05d10a4cabf10bec24f
cpsinghal <chetansinghal1988@gmail.com>
2013-03-16 18:15:16 +0530

    formatting changes

--- README.txt
+++ README.txt
@@ -1,15 +1,28 @@
+Description
+-----------
+This module adds a webform matrix component in webform
+components list.In webform matrix component you 
+can add as many as rows and columns. Subcomponent
+of matrix component is Textfield, Select , Label ,Date.
+This module also manage webform Results page for
+webform matrix.Values are save in database is serialize form.
 
-Webform Matrix Component
+Requirements
+------------
+Drupal 7.x
+Webform module
 
-************************
+Installation
+------------
+1. Copy the entire webform_matrix_component directory 
+   the Drupal sites/all/modules directory.
 
-This module adds the ability to create matrix component.
-This module also give flaxibility to choose any type of element like textfield,select,label,date
+2. Login as an administrator. Enable the module in the
+  "Administer" -> "Modules"
 
-Admin settings-admin/config/content/webform
-You can set max no row and max no columns.
+3. (Optional) Edit the settings under "Administer" -> "Configuration" ->
+   "Content authoring" -> "Webform settings"
 
-
-
+4. Now matrix component is show in component list.
 
 
--- webform_matrix_component.api.php
+++ webform_matrix_component.api.php
@@ -1,19 +1,14 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * validate hook for matrix component
  * you can define custom validation for this
- * @param type $element
- * @param type $form_state
+ * @param $element element
+ * @param $form_state form_state
  * 
  */
 function hook_webform_matrix_validate($element, $form_state) {
-    
+  
 }
 
 /**
@@ -21,12 +16,12 @@ function hook_webform_matrix_validate($element, $form_state) {
  * @see webform_render_matrix_textfield_Numeric
  */
 function webform_render_matrix_textfield_Numeric($element, $form_state) {
-    $value = $element['#value'];
-    if (!empty($value)) {
-        if (!is_numeric($value)) {
-            form_error($element, t('Only Numeric value is allowed'));
-        }
+  $value = $element['#value'];
+  if (!empty($value)) {
+    if (!is_numeric($value)) {
+      form_error($element, t('Only Numeric value is allowed'));
     }
+  }
 }
 
 /**
@@ -34,7 +29,7 @@ function webform_render_matrix_textfield_Numeric($element, $form_state) {
  * @see webform_render_matrix_textfield_Numeric
  */
 function webform_render_matrix_textfield_Alphabet($element, $form_state) {
-    
+  
 }
 
 /**
@@ -47,15 +42,15 @@ function webform_render_matrix_textfield_Alphabet($element, $form_state) {
  * 
  */
 function hook_webform_edit_matrix_alter(&$element, &$element_values, $element_id) {
-    $helptext = $element_values['helptext'];
-    $element['helptext'] = array(
-        '#type' => 'textfield',
-        '#title' => 'Help Text',
-        '#size' => 15,
-        '#default_value' => $helptext,
-        '#parents' => array('extra', 'element', $element_id, 'helptext'),
-        '#weight' => 1,
-    );
+  $helptext = $element_values['helptext'];
+  $element['helptext'] = array(
+      '#type' => 'textfield',
+      '#title' => 'Help Text',
+      '#size' => 15,
+      '#default_value' => $helptext,
+      '#parents' => array('extra', 'element', $element_id, 'helptext'),
+      '#weight' => 1,
+  );
 }
 
 /**
@@ -66,13 +61,10 @@ function hook_webform_edit_matrix_alter(&$element, &$element_values, $element_id
  * @param type $element
  */
 function hook_webform_render_matrix_alter(&$component, &$value, &$element) {
-    $helptext = $value['helptext'];
-    if (!empty($helptext)) {
-        $element['#field_suffix'] = $helptext;
-    }
+  $helptext = $value['helptext'];
+  if (!empty($helptext)) {
+    $element['#field_suffix'] = $helptext;
+  }
 }
-
-
-?>
 
 
