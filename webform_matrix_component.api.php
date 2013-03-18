4199dfcecba0a2322e4a01654fc9ef20d5cc3365
cpsinghal <chetansinghal1988@gmail.com>
2013-03-18 17:28:57 +0530

    indentation

--- webform_matrix_component.api.php
+++ webform_matrix_component.api.php
@@ -10,7 +10,6 @@
  *   form_state
  */
 function hook_webform_matrix_validate($element, $form_state) {
-  
 }
 
 /**
@@ -31,7 +30,6 @@ function webform_render_matrix_textfield_Numeric($element, $form_state) {
  * @see webform_render_matrix_textfield_Numeric()
  */
 function webform_render_matrix_textfield_Alphabet($element, $form_state) {
-  
 }
 
 /**
@@ -39,11 +37,6 @@ function webform_render_matrix_textfield_Alphabet($element, $form_state) {
  * Webform_edit_matrix_alter for alter matrix edit form.
  * Like add helptext textfield.
  * @see hook_webform_render_matrix_alter
- * 
- * @param $element
- * @param $element_values
- * @param $element_id
- * 
  */
 function hook_webform_edit_matrix_alter(&$element, &$element_values, $element_id) {
   $helptext = $element_values['helptext'];
@@ -60,10 +53,6 @@ function hook_webform_edit_matrix_alter(&$element, &$element_values, $element_id
 /**
  * Webform_render_matrix_alter for alter webform_render_matrix .
  * @see hook_webform_edit_matrix_alter
- * 
- * @param $component
- * @param $value
- * @param $element
  */
 function hook_webform_render_matrix_alter(&$component, &$value, &$element) {
   $helptext = $value['helptext'];
@@ -71,5 +60,4 @@ function hook_webform_render_matrix_alter(&$component, &$value, &$element) {
     $element['#field_suffix'] = $helptext;
   }
 }
-
 
